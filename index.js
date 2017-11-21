4930df7c3095e2eabe6d4f6c36d7c59fff90ead4
Sam Meech-Ward <sam@meech-ward.com>
2017-11-21 11:05:24 -0800

    Setup a Withdrawal object
    Create a Withrawal object to enable withdrawing money from our balance.

---
+++ index.js
@@ -0,0 +1,23 @@
+let balance = 500.00;
+
+class Withdrawal {
+  
+  constructor(amount) {
+    this.amount = amount;
+  }
+
+  commit() {
+    balance -= this.amount;
+  }
+
+}
+
+t1 = new Withdrawal(50.25);
+t1.commit();
+console.log('Transaction 1: $', t1);
+
+t2 = new Withdrawal(9.99);
+t1.commit();
+console.log('Transaction 2: $', t2);
+
+console.log('Balance: $', balance);
